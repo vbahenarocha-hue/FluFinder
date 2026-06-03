24230cef54ecaf3980711856e092be4e5d93aa8a
vbahenarocha@ucsd.edu <vbahenarocha@ucsd.edu>
2026-06-03 11:27:25 -0700

    changes to function 1

--- Flufinder.R
+++ Flufinder.R
@@ -1,6 +1,5 @@
 upload_fasta <- function(fasta_filename) {
-  #Opening seqinr library for handling FASTA files; make sure you have
-  seqinr installed
+  #Opening seqinr library for handling FASTA files; make sure you have seqinr installed
   library(seqinr)
   #Reading the fasta file
   read.fasta(fasta_filename, seqtype = "AA", as.string = TRUE,
